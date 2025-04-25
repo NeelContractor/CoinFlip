d4b0a8f75c82dc76996637507cdb12579fc19b7b
NeelContractor <neelcontractor7@gmail.com>
2025-04-25 11:36:01 +0530

    fixed linting errors

--- src/components/AppWalletProvider.tsx
+++ src/components/AppWalletProvider.tsx
@@ -1,18 +1,13 @@
 "use client";
 
-import React, { useMemo } from "react";
+import React from "react";
 import {
   ConnectionProvider,
   WalletProvider,
 } from "@solana/wallet-adapter-react";
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { clusterApiUrl } from "@solana/web3.js";
 
 import"@solana/wallet-adapter-react-ui/styles.css";
-
-
-// imports here
 
 export default function AppWalletProvider({
     children,
